8190bc694920f0a3d9809d4d5a1141c2a3d72594
BongsoonChoi <willbsoon16@gmail.com>
2021-10-20 17:26:51 +0900

    가이드 수정

--- screenblock.py
+++ screenblock.py
@@ -10,12 +10,16 @@ from datetime import datetime
 
 
 CNT = 1
-FLAG = 0
 TIME_INTERVAL = 500
 try:
     TIME_INTERVAL = int(input('기준 시간을 입력하세요(초 단위). ==> ') )   # second
-except:
+    if TIME_INTERVAL >= 1200:
+        raise Exception('최대시간은 1000초 입니다.')
+except ValueError:
     sys.exit('숫자 외 다른 문자를 입력하셨습니다. 종료합니다.')
+except Exception as e:
+    sys.exit(e.__str__() +' 종료합니다.')
+
 PASS = 'pass'
 BLOCK = 'block'
 
@@ -51,11 +55,11 @@ def printStr(screenWidth, screenHeight, currentMouseX, currentMouseY, blockType)
     stdscr.addstr(20,0,'===    ====  =======  =====  =====    ==  =====  ==        ============      ====        ====    ======     ===  ====  =')
     stdscr.addstr(21,0,'========================================================================================================================')
 
-    stdscr.addstr(22,0,'ON-TIME block 실 행 ~~! ( {} sec 기 준 )'.format(TIME_INTERVAL))
+    stdscr.addstr(22,0,'ON-TIME block 실 행 ~~! ( 기 준 시 간  : {} sec )'.format(TIME_INTERVAL))
     stdscr.addstr(23,0,'전 체 사 이 즈 : {:>4}, {:>4} / 현 재  위 치 : {:>4}, {:>4} '
               .format(screenWidth, screenHeight, currentMouseX, currentMouseY))
     
-    stdscr.addstr(24,0,'기 준  시 간 에  따 라  마 우 스  위 치  변 화 를  체 크 해 서  변 화 가  없 으 면  하 단 부 를  클 릭 해 서  온 타 임  작 동 을  무 력 화  합 니 다. ')
+    stdscr.addstr(24,0,'기 준  시 간 에  따 라  마 우 스  위 치  변 화 를  체 크 해 서  기 준 시 간 동 안  변 화 가  없 으 면  하 단 부 를  클 릭 해 서  온 타 임  작 동 을  무 력 화  합 니 다. ')
     if blockType ==  PASS:
         stdscr.addstr(26,0,' '*100)
         stdscr.addstr(26,0,'현 재 위 치 : {:>4}, {:>4} // pass! '.format(currentMouseX, currentMouseY))
@@ -63,8 +67,8 @@ def printStr(screenWidth, screenHeight, currentMouseX, currentMouseY, blockType)
         stdscr.addstr(26,0,' '*100)
         stdscr.addstr(26,0,'[{}] {:0>4} 번 째  동 작 /// ({:>4},{:>4}) 위 치  클 릭 '
                           .format(datetime.now(), CNT, int(screenWidth*4/5), screenHeight))
-    stdscr.addstr(27,0,'경 과 시 간 : {:>2}min {:>2}sec '.format(int(timer/60), timer%60))
-    stdscr.addstr(28,0,'종 료 를  원 하 시 면  창 을  종 료 하 세 요. ')
+    stdscr.addstr(27,0,'경 과 시 간 : {:>2}min {:>2}sec /// {}sec '.format(int(timer/60), timer%60, timer))
+    stdscr.addstr(28,0,'중 단 을   원 하 시 면   창 을   종 료 하 세 요 ')
     stdscr.refresh()
     
 
